75a5806db9548cc3529e87a18442104997a2b95b
Nicky <nickyliu91@gmail.com>
2022-09-17 21:48:09 -0400

    added all basic rails models and controllers

---
+++ db/migrate/20220918014508_create_accounts.rb
@@ -0,0 +1,12 @@
+class CreateAccounts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :accounts do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+      t.string :rank
+
+      t.timestamps
+    end
+  end
+end
