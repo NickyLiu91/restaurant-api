8827b4da7aab29d098875d750a24d7cffc41c82b
Nicky <nickyliu91@gmail.com>
2022-10-11 05:54:40 -0400

    added restaurant id to order, added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+account1 = Account.new(name: "Account 1", email: "test1@gmail.com", phone: 1111111,
+        rank: "manager", password: "testpassword")
+
+restaurant1 = Restaurant.new(name: "Restaurant 1", location: "Narnia", account_id: account1.id)
+
+order1 = Order.new(items: [{"chicken": 1.00 }, {"bread": 2.00}, {"Rice": 5.00}], restaurant_id: restaurant1.id)
