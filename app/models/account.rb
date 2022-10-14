50f3ab2809ecc3f34602de9fdb8f77683042cc85
Nicky <nickyliu91@gmail.com>
2022-10-14 11:22:23 -0400

    aliased accounts

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,7 +2,7 @@ class Account < ApplicationRecord
   has_secure_password
   validates :name, uniqueness: { case_sensitive: false}
 
-  belongs_to :restaurant, optional: true
+  belongs_to :sourceRestaurant, class_name: 'Restaurant', optional: true
 
   has_one :restaurant
 end
--- app/serializers/account_serializer.rb
+++ app/serializers/account_serializer.rb
@@ -1,6 +1,6 @@
 class AccountSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :phone, :rank
+  attributes :id, :name, :email, :phone, :rank, :sourceRestaurant
 
   has_one :restaurant
-  belongs_to :restaurant, optional: true
+  belongs_to :sourceRestaurant, optional: true
 end
